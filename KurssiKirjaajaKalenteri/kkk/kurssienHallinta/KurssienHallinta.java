e80b808f93885f98429a1e675efeb6c11e543118
Mapukto <maot@cs.helsinki.fi>
2015-05-12 17:49:06 +0300

    ohjelmoinnin aloitus

---
+++ KurssiKirjaajaKalenteri/kkk/kurssienHallinta/KurssienHallinta.java
@@ -0,0 +1,9 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import KurssiKirjaajaKalenteri.kkk.kurssi.Kurssi;
+
+public class KurssienHallinta {
+    List<Kurssi> kurssit = new ArrayList<Kurssi>;
+    
+}
